03e2a9a7a6e49be5649b600df2a56eaddff35893
Siebrand Mazeland <siebrand@kitano.nl>
2014-04-01 16:49:08 +0200

    Migrate to JSON i18n
    Procedure per https://www.mediawiki.org/wiki/Manual:GenerateJsonI18n.php
    with shim.
    
    Change-Id: I59a0eb57c01c962274c21efc1532b77c9becfa09

--- YotpoReviews.php
+++ YotpoReviews.php
@@ -4,11 +4,12 @@ $wgExtensionCredits['parserhook'][] = array(
 	'path'           => __FILE__,
 	'name'           => 'YotpoReviews',
 	'descriptionmsg' => 'yotporeviews-desc',
-	'version'        => 0.1,
+	'version'        => '0.2.0',
 	'author'         => 'Luis Felipe Schenone',
 	'url'            => 'https://www.mediawiki.org/wiki/Extension:YotpoReviews'
 );
 
+$wgMessagesDirs['YotpoReviews'] = __DIR__ . '/i18n';
 $wgExtensionMessagesFiles['YotpoReviews'] = __DIR__ . '/YotpoReviews.i18n.php';
 $wgAutoloadClasses['YotpoReviews'] = __DIR__ . '/YotpoReviews.body.php';
 
