287fe2c0341a35ce6e242cc7afdee78f0d957bdc
Sophivorus <schenonef@gmail.com>
2018-02-11 02:21:24 -0300

    Add extension registration
    Also update licence to GPL-3.0
    and minor code improvements
    
    Change-Id: I6e926ab1fd8c2b6ea78e1884c3c31cbf5811a920

--- YotpoReviews.php
+++ YotpoReviews.php
@@ -1,18 +1,57 @@
 <?php
 
-$wgExtensionCredits['parserhook'][] = array(
-	'path'           => __FILE__,
-	'name'           => 'YotpoReviews',
-	'namemsg'        => 'yotporeviews-extensionname',
-	'descriptionmsg' => 'yotporeviews-desc',
-	'version'        => '0.3',
-	'author'         => '[https://www.mediawiki.org/wiki/User:Luis_Felipe_Schenone Luis Felipe Schenone]',
-	'url'            => 'http://www.mediawiki.org/wiki/Extension:YotpoReviews',
-	'license-name'   => 'GPL-2.0+',
-);
+class YotpoReviews {
 
-$wgMessagesDirs['YotpoReviews'] = __DIR__ . '/i18n';
-$wgAutoloadClasses['YotpoReviews'] = __DIR__ . '/YotpoReviews.body.php';
+	public static function onParserFirstCallInit( Parser &$parser ) {
+		$parser->setHook( 'yotporeviews', 'YotpoReviews::render' );
+		$parser->setHook( 'yotpo-reviews', 'YotpoReviews::render' ); // Backwards compatibility
+		return true;
+	}
 
-$wgHooks['ParserFirstCallInit'][] = 'YotpoReviews::onParserFirstCallInit';
-$wgHooks['SkinAfterContent'][] = 'YotpoReviews::onSkinAfterContent';
+	public static function onSkinAfterContent( &$data, Skin $skin ) {
+		global $wgYotpoAppKey;
+		$data .= '<script>(function e(){var e=document.createElement("script");e.type="text/javascript",e.async=true,e.src="//staticw2.yotpo.com/' . $wgYotpoAppKey . '/widget.js";var t=document.getElementsByTagName("script")[0];t.parentNode.insertBefore(e,t)})();</script>';
+		return true;
+	}
+
+	public static function render( $input, array $args, Parser $parser, PPFrame $frame ) {
+
+		$width = '100%'; // Default
+		if ( array_key_exists( 'width', $args ) ) {
+			$width = $args['width'] . 'px';
+		}
+
+		$height = '100%'; // Default
+		if ( array_key_exists( 'height', $args ) ) {
+			$height = $args['height'] . 'px';
+		}
+
+		$align = 'center'; // Default
+		if ( array_key_exists( 'align', $args ) ) {
+			$align = $args['align'];
+		}
+		if ( $align === 'left' ) {
+			$margin = '0 auto 0 0';
+		}
+		if ( $align === 'right' ) {
+			$margin = '0 0 0 auto';
+		}
+		if ( $align === 'center' ) {
+			$margin = '0 auto 0 auto';
+		}
+
+		$float = 'none'; // Default
+		if ( array_key_exists( 'float', $args ) ) {
+			$float = $args['float'];
+		}
+
+		$title = $parser->getTitle();
+		$data_product_id = $title->getDBKey();
+		$data_name = $title->getText();
+		$data_url = $title->getFullURL();
+		$data_image_url = '';
+		$data_description = '';
+
+		return '<div style="float:' . $float . '; margin:' . $margin . '; width:' . $width . '; height:' . $height . ';" class="yotpo yotpo-main-widget" data-product-id="' . $data_product_id . '" data-name="' . $data_name . '" data-url="' . $data_url . '" data-image-url="' . $data_image_url . '" data-description="' . $data_description . '"></div>';
+	}
+}
